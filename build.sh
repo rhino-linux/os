16599840365bf5c738ed78d4a8a56e337134bf31
Alejandro Cervera <96702705+tricantivu@users.noreply.github.com>
2023-02-06 20:41:01 -0500

    style(build.sh): wrap parameter expansions around braces
    Except positional parameters or command line arguments (e.g $1, $2, $3...)

--- build.sh
+++ build.sh
@@ -16,8 +16,8 @@ if [[ -n "$1" ]]; then
 else
   CONFIG_FILE="etc/terraform.conf"
 fi
-BASE_DIR="$PWD"
-source "$BASE_DIR"/"$CONFIG_FILE"
+BASE_DIR="${PWD}"
+source "${BASE_DIR}"/"${CONFIG_FILE}"
 
 echo -e "
 #----------------------#
@@ -32,17 +32,17 @@ ln -sfn /usr/share/debootstrap/scripts/gutsy /usr/share/debootstrap/scripts/deve
 build () {
   BUILD_ARCH="$1"
 
-  mkdir -p "$BASE_DIR/tmp/$BUILD_ARCH"
-  cd "$BASE_DIR/tmp/$BUILD_ARCH" || exit
+  mkdir -p "${BASE_DIR}/tmp/${BUILD_ARCH}"
+  cd "${BASE_DIR}/tmp/${BUILD_ARCH}" || exit
 
   # remove old configs and copy over new
   rm -rf config auto
-  cp -r "$BASE_DIR"/etc/* .
+  cp -r "${BASE_DIR}"/etc/* .
   # Make sure conffile specified as arg has correct name
-  cp -f "$BASE_DIR"/"$CONFIG_FILE" terraform.conf
+  cp -f "${BASE_DIR}"/"${CONFIG_FILE}" terraform.conf
 
   # Symlink chosen package lists to where live-build will find them
-  ln -s "package-lists.$PACKAGE_LISTS_SUFFIX" "config/package-lists"
+  ln -s "package-lists.${PACKAGE_LISTS_SUFFIX}" "config/package-lists"
 
   echo -e "
 #------------------#
@@ -71,22 +71,22 @@ build () {
 #---------------------------#
 "
 
-  OUTPUT_DIR="$BASE_DIR/builds/$BUILD_ARCH"
-  mkdir -p "$OUTPUT_DIR"
-  FNAME="Rolling-Linux-OS-$VERSION$OUTPUT_SUFFIX"
-  mv "$BASE_DIR/tmp/$BUILD_ARCH/live-image-$BUILD_ARCH.hybrid.iso" "$OUTPUT_DIR/${FNAME}.iso"
+  OUTPUT_DIR="${BASE_DIR}/builds/${BUILD_ARCH}"
+  mkdir -p "${OUTPUT_DIR}"
+  FNAME="Rolling-Linux-OS-${VERSION}${OUTPUT_SUFFIX}"
+  mv "${BASE_DIR}/tmp/${BUILD_ARCH}/live-image-${BUILD_ARCH}.hybrid.iso" "${OUTPUT_DIR}/${FNAME}.iso"
 
   # cd into output to so {FNAME}.sha256.txt only
   # includes the filename and not the path to
   # our file.
-  cd $OUTPUT_DIR
+  cd ${OUTPUT_DIR}
   sha512sum "${FNAME}.iso" > "${FNAME}.sha512"
   sha256sum "${FNAME}.iso" > "${FNAME}.sha256"
-  cd $BASE_DIR
+  cd ${BASE_DIR}
 }
 
-if [[ "$ARCH" == "all" ]]; then
+if [[ "${ARCH}" == "all" ]]; then
     build amd64
 else
-    build "$ARCH"
+    build "${ARCH}"
 fi
