5b8d448e59f8195bd705666c79c295e4d869cd24
oklopfer <104327997+oklopfer@users.noreply.github.com>
2023-03-31 22:07:18 -0400

    Update server-clean.sh

--- scripts/server-clean.sh
+++ scripts/server-clean.sh
@@ -6,7 +6,7 @@ echo "nameserver 1.1.1.1" > /etc/resolv.conf
 export DEBIAN_FRONTEND=noninteractive
 export DEBCONF_NONINTERACTIVE_SEEN=true
 
-apt-get remove -yq gnome-disk-utility dconf-editor firefox codium ulauncher rhino-setup quintom-cursor-theme gparted network-manager-gnome lightdm lightdm-gtk-greeter thunar thunar-volman xfce4-goodies xfce4-appfinder xfce4-notifyd xfce4-panel xfce4-terminal xfce4-session xfce4-settings xfdesktop4 xfwm4 xorg x11-common mousepad qt5-style-kvantum qt5-style-kvantum-themes mugshot mpv
+apt-get remove -yq gnome-disk-utility dconf-editor firefox codium ulauncher rhino-setup quintom-cursor-theme gparted network-manager-gnome lightdm lightdm-gtk-greeter thunar thunar-volman xfce4-goodies xfce4-appfinder xfce4-notifyd xfce4-panel xfce4-terminal xfce4-session xfce4-settings xfdesktop4 xfwm4 xorg x11-common mousepad qt5-style-kvantum qt5-style-kvantum-themes mugshot mpv xubuntu-default-settings libglib2.0-bin
 
 echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 HOME=/home/rhino runuser -l rhino -c 'SUDO_USER=rhino PACSTALL_DOWNLOADER=quiet-wget pacstall -PI rhino-pkg-git rhino-neofetch-git'
