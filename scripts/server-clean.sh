43d2f9787039633969651d2252aa6acd55c935d9
oklopfer <104327997+oklopfer@users.noreply.github.com>
2023-04-01 10:28:59 -0400

    Update server-clean.sh

--- scripts/server-clean.sh
+++ scripts/server-clean.sh
@@ -8,11 +8,9 @@ export DEBCONF_NONINTERACTIVE_SEEN=true
 
 apt-get update
 apt-get remove -yq gnome-disk-utility dconf-editor firefox codium ulauncher rhino-setup quintom-cursor-theme network-manager-gnome lightdm lightdm-gtk-greeter thunar thunar-volman xfce4-goodies xfce4-appfinder xfce4-notifyd xfce4-panel xfce4-terminal xfce4-session xfce4-settings xfdesktop4 xfwm4 xorg x11-common mousepad qt5-style-kvantum qt5-style-kvantum-themes mugshot mpv xubuntu-default-settings libglib2.0-bin
-
 echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 HOME=/home/rhino runuser -l rhino -c 'SUDO_USER=rhino PACSTALL_DOWNLOADER=quiet-wget pacstall -PI rhino-pkg-git rhino-neofetch-git'
 sed -i 's/%sudo ALL=(ALL) NOPASSWD:ALL//' /etc/sudoers
-
 apt-get autoremove -y
 
 rm /etc/resolv.conf
