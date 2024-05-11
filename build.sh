b9309e56487fbd033ce29b009e7c3d956727a3f1
oklopfer <104327997+oklopfer@users.noreply.github.com>
2024-05-11 01:48:08 +0000

    mount directly

--- build.sh
+++ build.sh
@@ -80,7 +80,10 @@ build () {
 # LIVE-BUILD BUILD #
 #------------------#
 "
+  mkdir -p tmp/$BUILD_ARCH/chroot
+  mount --bind tmp/$BUILD_ARCH/chroot tmp/$BUILD_ARCH/chroot
   lb --force build
+  umount tmp/$BUILD_ARCH/chroot
 
   echo -e "
 #---------------------------#
