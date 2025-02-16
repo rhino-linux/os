69e38ae996a06f305d33ae39d593a9c15d2df2d2
oklopfer <104327997+oklopfer@users.noreply.github.com>
2025-02-16 15:50:13 -0500

    really really fix resize

--- etc/config/includes.chroot/usr/sbin/resizefs.sh
+++ etc/config/includes.chroot/usr/sbin/resizefs.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
 loc=$(df -P . | sed -n '$s/[[:blank:]].*//p')
+dev=${loc%%p*}
+part=${loc##*p}
 
 /usr/bin/expect <<EOF
-spawn /usr/sbin/parted ${loc%%p*} resizepart ${loc##*p} 100%
-expect "Yes/No?"
-send "Yes\r"
-expect eof
+spawn /usr/sbin/parted ${dev} print
+expect {
+    "Fix/Ignore?" {
+        send "Fix\r"
+        exp_continue
+    }
+    eof
+}
+
+spawn /usr/sbin/parted ${dev} resizepart ${part} 100%
+expect {
+    "Yes/No?" {
+        send "Yes\r"
+        exp_continue
+    }
+    eof
+}
 EOF
 
 /usr/sbin/resize2fs ${loc}
