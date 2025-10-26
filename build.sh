7e2d4bb9b7380076946754ef35eaa6bd3eaa194e
Elsie Wenger-Stickel <elsie19@pm.me>
2025-10-26 16:19:35 -0400

    fix(*): use if guards instead of teeing

--- build.sh
+++ build.sh
@@ -4,34 +4,18 @@ set -e
 
 # check for root permissions
 if [[ "$(id -u)" != 0 ]]; then
-  echo "E: Requires root permissions" > /dev/stderr
-  exit 1
+    echo "E: Requires root permissions" >/dev/stderr
+    exit 1
 fi
 
 # get config
 if [ -n "$1" ]; then
-  CONFIG_FILE="$1"
+    CONFIG_FILE="$1"
 else
-  CONFIG_FILE="etc/terraform.conf"
+    CONFIG_FILE="etc/terraform.conf"
 fi
 BASE_DIR="$PWD"
 source "$BASE_DIR"/"$CONFIG_FILE"
-
-# do arch-dep adjustment
-if [ $ARCH = arm64 ]; then
-echo 'initramfs-tools
-grub-efi-arm64
-grub-efi-arm64-bin
-grub-efi-arm64-signed' | sudo tee -a etc/config/package-lists.calamares/pool.list.binary; 
-else
-echo 'bcmwl-kernel-source
-microcode-initrd
-intel-microcode
-iucode-tool
-grub-efi-amd64
-grub-efi-amd64-bin
-grub-efi-amd64-signed' | sudo tee -a etc/config/package-lists.calamares/pool.list.binary; 
-fi
 
 #VanillaOS patch to yeet ia32
 #sudo sed -i '/Check_package chroot \/usr\/lib\/grub\/i386-efi\/configfile.mod grub-efi-ia32-bin/d' /usr/lib/live/build/binary_grub-efi
@@ -43,66 +27,66 @@ echo -e "
 "
 
 apt-get update
-apt-get install -y patch gnupg2 binutils zstd ubuntu-keyring libglib2.0-dev libmysqlclient-dev 
+apt-get install -y patch gnupg2 binutils zstd ubuntu-keyring libglib2.0-dev libmysqlclient-dev
 apt-get install -y apt-utils
 ln -sfn /usr/share/debootstrap/scripts/gutsy /usr/share/debootstrap/scripts/devel
 
-build () {
-  BUILD_ARCH="$1"
+build() {
+    BUILD_ARCH="$1"
 
-  mkdir -p "$BASE_DIR/tmp/$BUILD_ARCH"
-  cd "$BASE_DIR/tmp/$BUILD_ARCH" || exit
+    mkdir -p "$BASE_DIR/tmp/$BUILD_ARCH"
+    cd "$BASE_DIR/tmp/$BUILD_ARCH" || exit
 
-  # remove old configs and copy over new
-  rm -rf config auto
-  cp -r "$BASE_DIR"/etc/* .
-  # Make sure conffile specified as arg has correct name
-  cp -f "$BASE_DIR"/"$CONFIG_FILE" terraform.conf
+    # remove old configs and copy over new
+    rm -rf config auto
+    cp -r "$BASE_DIR"/etc/* .
+    # Make sure conffile specified as arg has correct name
+    cp -f "$BASE_DIR"/"$CONFIG_FILE" terraform.conf
 
-  # Symlink chosen package lists to where live-build will find them
-  ln -s "package-lists.$PACKAGE_LISTS_SUFFIX" "config/package-lists"
+    # Symlink chosen package lists to where live-build will find them
+    ln -s "package-lists.$PACKAGE_LISTS_SUFFIX" "config/package-lists"
 
-  echo -e "
+    echo -e "
 #------------------#
 # LIVE-BUILD CLEAN #
 #------------------#
 "
-  lb clean
+    lb clean
 
-  echo -e "
+    echo -e "
 #-------------------#
 # LIVE-BUILD CONFIG #
 #-------------------#
 "
-  lb config
+    lb config
 
-  echo -e "
+    echo -e "
 #------------------#
 # LIVE-BUILD BUILD #
 #------------------#
 "
-  lb --force build
+    lb --force build
 
-  echo -e "
+    echo -e "
 #---------------------------#
 # MOVE OUTPUT TO BUILDS DIR #
 #---------------------------#
 "
 
-  OUTPUT_DIR="$BASE_DIR/builds/$BUILD_ARCH"
-  mkdir -p "$OUTPUT_DIR"
-  mv "$BASE_DIR/tmp/$BUILD_ARCH/live-image-$BUILD_ARCH.hybrid.iso" "$OUTPUT_DIR/${FNAME}.iso"
+    OUTPUT_DIR="$BASE_DIR/builds/$BUILD_ARCH"
+    mkdir -p "$OUTPUT_DIR"
+    mv "$BASE_DIR/tmp/$BUILD_ARCH/live-image-$BUILD_ARCH.hybrid.iso" "$OUTPUT_DIR/${FNAME}.iso"
 
-  # cd into output to so {FNAME}.sha256.txt only
-  # includes the filename and not the path to
-  # our file.
-  cd $OUTPUT_DIR
-  sha512sum "${FNAME}.iso" > "${FNAME}.sha512"
-  sha256sum "${FNAME}.iso" > "${FNAME}.sha256"
-  cd $BASE_DIR
+    # cd into output to so {FNAME}.sha256.txt only
+    # includes the filename and not the path to
+    # our file.
+    cd $OUTPUT_DIR
+    sha512sum "${FNAME}.iso" >"${FNAME}.sha512"
+    sha256sum "${FNAME}.iso" >"${FNAME}.sha256"
+    cd $BASE_DIR
 }
 
-cat > etc/config/package-lists.calamares/pool.list.binary << __EOF__
+cat >etc/config/package-lists.calamares/pool.list.binary <<__EOF__
 b43-fwcutter
 dkms
 setserial
