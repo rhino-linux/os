ab892a971573465233cfb0fc4f896f5af4c5117a
ook37 <oren+12345@taumoda.com>
2026-07-18 21:00:47 -0400

    my attempt

---
+++ platform/pine64/base/build.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+set -e
+
+# check for root permissions
+if [[ "$(id -u)" != 0 ]]; then
+  echo "E: Requires root permissions" > /dev/stderr
+  exit 1
+fi
+
+# get config
+if [ -n "$1" ]; then
+  CONFIG_FILE="$1"
+else
+  CONFIG_FILE="etc/terraform.conf"
+fi
+BASE_DIR="$PWD"
+source "$BASE_DIR"/"$CONFIG_FILE"
+
+
+#VanillaOS patch to yeet ia32
+#sudo sed -i '/Check_package chroot \/usr\/lib\/grub\/i386-efi\/configfile.mod grub-efi-ia32-bin/d' /usr/lib/live/build/binary_grub-efi
+
+echo -e "
+#----------------------#
+# INSTALL DEPENDENCIES #
+#----------------------#
+"
+
+apt-get update
+apt-get install -y patch gnupg2 binutils zstd ubuntu-keyring libglib2.0-dev libmysqlclient-dev 
+apt-get install -y apt-utils
+ln -sfn /usr/share/debootstrap/scripts/gutsy /usr/share/debootstrap/scripts/devel
+
+build () {
+  BUILD_ARCH="$1"
+
+  mkdir -p "$BASE_DIR/tmp/$BUILD_ARCH"
+  cd "$BASE_DIR/tmp/$BUILD_ARCH" || exit
+
+  # remove old configs and copy over new
+  rm -rf config auto
+  cp -r "$BASE_DIR"/etc/* .
+  # Make sure conffile specified as arg has correct name
+  cp -f "$BASE_DIR"/"$CONFIG_FILE" terraform.conf
+
+  # Symlink chosen package lists to where live-build will find them
+  ln -s "package-lists.$PACKAGE_LISTS_SUFFIX" "config/package-lists"
+
+  echo -e "
+#------------------#
+# LIVE-BUILD CLEAN #
+#------------------#
+"
+  lb clean
+
+  echo -e "
+#-------------------#
+# LIVE-BUILD CONFIG #
+#-------------------#
+"
+  lb config
+
+  echo -e "
+#------------------#
+# LIVE-BUILD BUILD #
+#------------------#
+"
+  lb --force build
+
+  echo -e "
+#---------------------------#
+# MOVE OUTPUT TO BUILDS DIR #
+#---------------------------#
+"
+
+  OUTPUT_DIR="$BASE_DIR/binary"
+  mkdir -p "$OUTPUT_DIR"
+  mv "$BASE_DIR/tmp/$BUILD_ARCH/"*.tar.tar "$OUTPUT_DIR/${FNAME}.tar"
+
+  # cd into output to so {FNAME}.sha256.txt only
+  # includes the filename and not the path to
+  # our file.
+  cd $OUTPUT_DIR
+  sha512sum "${FNAME}.tar" > "${FNAME}.sha512"
+  sha256sum "${FNAME}.tar" > "${FNAME}.sha256"
+  cd $BASE_DIR
+}
+
+
+if [[ "$ARCH" == "all" ]]; then
+    build amd64
+else
+    build "$ARCH"
+fi
