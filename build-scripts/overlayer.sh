d400751f5e490c6fe329dd258c86d8908dad4070
ook37 <oren+12345@taumoda.com>
2026-07-20 17:02:04 -0400

    add basic overlayer script

---
+++ build-scripts/overlayer.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+platform=${1:?Platform required}
+envir=${2:?Environment required}
+builddir=${3:?Build directory required}
+
+case ${platform} in
+  amd64|arm64)
+    overlays=({base,platform/iso-generic}/{base,environment/${envir}}/\*)
+  ;;
+  raspberrypi|raspi|rpi)
+    if [[ ${envir} == "server" ]]; then
+      overlays=({base,platform/{img-preinst,rpi}}/base/\*)
+    else
+      overlays=({base,platform/{img-preinst,rpi}}/{base,environment/${envir}}/\*)
+    fi
+  ;;
+  pinephone|pp|ppog|pinephonepro|ppp)
+    overlays=({base,platform/{img-preinst,pine64{,/phone}}}/{base,environment/${envir}}/\*)
+  ;;
+  pinetab|pt|ptog|pt1|pinetab2|pt2)
+    overlays=({base,platform/{img-preinst,pine64{,/tab}}}/{base,environment/${envir}}/\*)
+  ;;
+  *)
+    echo "Unknown platform, exiting"
+    exit 1
+  ;;
+esac
+
+mkdir -p "${builddir}"
+echo "Overlaying: cp -r ${overlays[@]} -t ${builddir}"
+cp -r ${overlays[@]} -t ${builddir}
