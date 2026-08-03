19e0a0b3af511d0b1509a68cd9c864721a3a692c
oklopfer <104327997+oklopfer@users.noreply.github.com>
2026-08-03 13:45:03 -0400

    fix bad rollback

--- deploy.sh
+++ deploy.sh
@@ -89,10 +89,9 @@ source "${builddir}/etc/terraform.conf"
 
 # check for root filesystem tarball
 shopt -s nullglob
-tarballs=("${builddir}"/binary/Rhino*.tar)
-shopt -u nullglob
-if (( ${#tarballs[@]} != 1 )); then
-  echo "E: Expected one root tarball in ${builddir}/binary. Exiting..." > /dev/stderr
+tarball="${builddir}/binary/${FNAME}.tar"
+if ! [[ -f ${tarball} ]]; then
+  echo "E: Root tarball not found, please run build.sh first and ensure output is placed in ${builddir}/binary. Exiting..."  > /dev/stderr
   exit 1
 fi
 
